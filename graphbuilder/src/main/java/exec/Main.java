87eaf07a2b0b2d1cef67ff0828bf08b660061900
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-02-14 04:11:13 -0500

    Updated Main class for graphbuilder

--- graphbuilder/src/main/java/exec/Main.java
+++ graphbuilder/src/main/java/exec/Main.java
@@ -1,11 +1,30 @@
 package exec;
 
+import java.util.List;
+
+import data.Author;
+import db.csv.CSVAccessLayer;
+import db.neo4j.Neo4jAccessLayer;
+import db.sql.SQLAccessLayer;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		createAuthorNodes();
 		
+	}
+	
+	public static void createAuthorNodes()
+	{
+		SQLAccessLayer sqlAccessLayer = new SQLAccessLayer();
+		List<Author> authorList = sqlAccessLayer.getUniqueAuthorList();
+		
+		Neo4jAccessLayer neo4jAccessLayer = new Neo4jAccessLayer();
+		neo4jAccessLayer.addAuthorNodes(authorList);
+		
+		CSVAccessLayer csvAccessLayer = new CSVAccessLayer();
+		csvAccessLayer.writeAuthorsToCSV(authorList);
 	}
 
 }
