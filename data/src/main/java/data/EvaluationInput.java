81fc3856c5d34dfe78751d4a68e61c5d5af2b9c4
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-02-27 02:27:57 -0500

    Create Data Structure to hold Evaluation Input

---
+++ data/src/main/java/data/EvaluationInput.java
@@ -0,0 +1,44 @@
+package data;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.apache.commons.collections4.map.MultiValueMap;
+
+public class EvaluationInput {
+
+private MultiValueMap<String, Integer> authorIdToProcIdMap;
+	
+	public EvaluationInput()
+	{
+		this.authorIdToProcIdMap = new MultiValueMap<String, Integer>();
+	}
+	
+	public Set<String> getAuthorIds()
+	{
+		return this.authorIdToProcIdMap.keySet();
+	}
+	
+	public List<Integer> getListOfProceedingIds(String authorId)
+	{
+		return (List<Integer>) this.authorIdToProcIdMap.get(authorId);
+	}
+	
+	public void addEntry(String authorId, Integer proceedingId)
+	{
+		this.authorIdToProcIdMap.put(authorId, proceedingId);
+	}
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return this.authorIdToProcIdMap.size();
+	}
+	
+	public Iterator<Entry<String, Integer>> getIterator()
+	{
+		return this.authorIdToProcIdMap.iterator();
+	}
+	
+}
