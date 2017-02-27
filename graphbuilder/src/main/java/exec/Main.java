ed8932ea62854e66f668b1438974db05bcf3fe8e
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-02-27 02:31:15 -0500

    Commented code in Main for graph builder to avoid overwriting the graph

--- graphbuilder/src/main/java/exec/Main.java
+++ graphbuilder/src/main/java/exec/Main.java
@@ -37,7 +37,7 @@ public class Main {
 		
 //		connectPaperAndReferencePaper();
 		
-		connectPaperAndJournal();
+//		connectPaperAndJournal();
 		
 		//connectProceedingAndPaper();
 //		MultiValueMap<Integer, Integer> a = new MultiValueMap<>();
