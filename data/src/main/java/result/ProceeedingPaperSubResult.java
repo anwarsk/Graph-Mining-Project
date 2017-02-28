ac15d4473ea0ae27a43ae7efb7ba5bc1ca7bbacd
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-02-28 17:02:32 -0500

    Added and connected data structure of results

--- data/src/main/java/result/ProceeedingPaperSubResult.java
+++ data/src/main/java/result/ProceeedingPaperSubResult.java
@@ -1,5 +1,12 @@
 package result;
 
-public class ProceeedingPaperSubResult {
+import java.util.Collections;
+import java.util.PriorityQueue;
 
+public class ProceeedingPaperSubResult {
+	
+	int proceedingArticleId;
+	double score;
+	
+	PriorityQueue<AuthorPaperSubResult> keywords = new PriorityQueue<AuthorPaperSubResult>(20, Collections.reverseOrder());
 }
