fbef0d4a0be496e6d1b941d92cb4348507faf905
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-02-28 17:57:01 -0500

    Added methods for result operations in the class

--- data/src/main/java/result/Result.java
+++ data/src/main/java/result/Result.java
@@ -8,10 +8,21 @@ import org.apache.commons.collections4.map.MultiValueMap;
 
 public class Result {
 	
+	
+
 	private String authorId;
 	private int proceedingId;
 	
-	PriorityQueue<ProceeedingPaperSubResult> keywords = new PriorityQueue<ProceeedingPaperSubResult>(20, Collections.reverseOrder());
-	//private Map<>
+	PriorityQueue<ProceeedingPaperSubResult> procPaperSubResutls;
 
+	public Result(String authorId, int proceedingId) {
+		
+		this.authorId = authorId;
+		this.proceedingId = proceedingId;
+		procPaperSubResutls = new PriorityQueue<ProceeedingPaperSubResult>(20, Collections.reverseOrder());
+	}
+
+	public void addProceedingPaperSubResult(ProceeedingPaperSubResult procPaperSubResult) {
+		this.procPaperSubResutls.add(procPaperSubResult);
+	}
 }
