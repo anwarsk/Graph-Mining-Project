146ded20f97ab3261cdd1cdee0ed04da2ed4cd96
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-02-28 17:56:22 -0500

    Added methods for result operations in the class

--- data/src/main/java/result/KeywordSubResult.java
+++ data/src/main/java/result/KeywordSubResult.java
@@ -1,8 +1,29 @@
 package result;
 
-public class KeywordSubResult {
+public class KeywordSubResult implements Comparable<KeywordSubResult>{
 
-	int keywordId;
-	double score;
 	
+	private int keywordId;
+	private double score;
+	
+	public KeywordSubResult(int keywordId, double score) {
+		this.keywordId = keywordId;
+		this.score = score;
+	}
+
+	public int compareTo(KeywordSubResult that) {
+		
+		int result = 0;
+		
+		if(this.score > that.score)
+		{
+			result = 1;
+		}
+		else if(this.score < that.score)
+		{
+			result = -1;
+		}
+		
+		return result;
+	}
 }
