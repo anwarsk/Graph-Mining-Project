a94b438ab6a30ede830a206f360a518ce6dac749
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-02-28 15:54:27 -0500

    Added preliminary code for weight calculation

--- analyzer/src/main/java/analyzer/WeightCalculator.java
+++ analyzer/src/main/java/analyzer/WeightCalculator.java
@@ -7,8 +7,7 @@ public class WeightCalculator {
 	public double getWeightForRelation(Relationship relation)
 	{
 		assert relation != null : "Null Relation";
-		relation.getProperty("f_weight");
-		return 0;
-		
+		double weight = (Double) relation.getProperty("f_weight");
+		return weight;
 	}
 }
