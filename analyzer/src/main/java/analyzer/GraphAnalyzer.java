29395d17ba01cdf313ad9a311c0cf463a04340ff
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-02-28 17:57:26 -0500

    Updated generateResult(..) method to create results

--- analyzer/src/main/java/analyzer/GraphAnalyzer.java
+++ analyzer/src/main/java/analyzer/GraphAnalyzer.java
@@ -1,7 +1,9 @@
 package analyzer;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 import org.neo4j.graphalgo.GraphAlgoFactory;
 import org.neo4j.graphalgo.PathFinder;
@@ -16,15 +18,21 @@ import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
+import result.AuthorPaperSubResult;
+import result.ProceeedingPaperSubResult;
+import result.Result;
+
+
 public class GraphAnalyzer {
 
-	private String NEO_GRAPH_DB_PATH = "";
+	private String NEO_GRAPH_DB_PATH = "./../../../Database/graph.acm2015";
 	private int MAX_TREE_DEPTH = 4;
 
 	public String generateResults(String authorId, int proceedingId)
 	{
 		assert authorId != null && authorId.isEmpty() == false : "Invalid Author Id";
 		assert proceedingId >0 : "Invalid Proceeding Id";
+		
 		File databaseDirectory = new File(NEO_GRAPH_DB_PATH);
 		GraphDatabaseService dbService =  new GraphDatabaseFactory().newEmbeddedDatabase(databaseDirectory);
 
@@ -41,26 +49,61 @@ public class GraphAnalyzer {
 			Iterator<Relationship> publishedRelations = proceeding.getRelationships(RelationshipType.withName("published_at")).iterator();
 
 			PathExpander<Object> pathExpander = this.createPathExpander();
+			WeightCalculator weightCalculator = new WeightCalculator();
 
+			Result result = new Result(authorId, proceedingId);
+			
 			while(publishedRelations.hasNext())
 			{
 				Node procPaperNode = publishedRelations.next().getStartNode();
+				int procArticleId = (int)(long) procPaperNode.getProperty("article_id"); 
+				ProceeedingPaperSubResult procPaperSubResult = new ProceeedingPaperSubResult(procArticleId);
+				
 				double procPaperScore = 0;
 
 				while(writtenRelations.hasNext())
 				{
-					double authorPaperScore =  1.0;
+					double authorPaperScore =  0;
 					Node authorPaperNode = writtenRelations.next().getEndNode();
 					
 					PathFinder<Path> allPathFinder = GraphAlgoFactory.allSimplePaths(pathExpander,MAX_TREE_DEPTH);
 					Iterable<Path> allPaths = allPathFinder.findAllPaths(authorPaperNode, procPaperNode);
+					Map<Integer, Double> keywordIdToScoreMap = new HashMap<Integer, Double>();
 					
 					for(Path path : allPaths)
 					{
-						//if()
+						double pathRWProbability = 1.0;
+						Iterable<Relationship> connections  = path.relationships();
+					
+						for(Relationship connection : connections)
+						{
+							double weight = weightCalculator.getWeightForRelation(connection);
+							pathRWProbability = pathRWProbability * weight;
+						}
+						authorPaperScore += pathRWProbability;
+						
+						Iterable<Node> nodes = path.nodes();
+						for(Node pathNode : nodes)
+						{
+							if(pathNode.hasLabel(Label.label("keyword")))
+							{
+								int keywordId = (int) pathNode.getProperty("keyword_id");
+								double currentKeywordScore = keywordIdToScoreMap.getOrDefault(keywordId, 0.0);
+								keywordIdToScoreMap.put(keywordId, currentKeywordScore + pathRWProbability);
+							}
+						}
 					}
-
+					// Create AuthorPaperSubResult
+					int authorArticleId = (int) (long) authorPaperNode.getProperty("article_id"); 
+					AuthorPaperSubResult authorPaperSubResult = new AuthorPaperSubResult(authorArticleId, authorPaperScore);
+					authorPaperSubResult.addKeywords(keywordIdToScoreMap);
+					
+					procPaperSubResult.addAuthorPaperSubResult(authorPaperSubResult);
+					procPaperScore += authorPaperScore;
 				}
+				
+				procPaperSubResult.setScore(procPaperScore);
+				result.addProceedingPaperSubResult(procPaperSubResult);
 			}
 
 			tx.success();
