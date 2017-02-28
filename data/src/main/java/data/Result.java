de52969bd9e1684d4cf2911e73a82cd89a5eb8ca
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-02-27 23:32:14 -0500

    Added data holders in Result

--- data/src/main/java/data/Result.java
+++ data/src/main/java/data/Result.java
@@ -1,5 +1,15 @@
 package data;
 
+import java.util.Map;
+
+import org.apache.commons.collections4.map.MultiValueMap;
+
 public class Result {
+	
+	private String authorId;
+	private int proceedingId;
+	private MultiValueMap<Integer, Integer> proceedingArticleIdToKeywordIdMap;
+	private Map<Integer, Float> proceedingArticleIdToScoreMap;
+	//private Map<>
 
 }
