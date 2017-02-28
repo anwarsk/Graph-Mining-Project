4b745ce983b07b68efc70936bf1de9893faadff5
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-02-28 17:56:34 -0500

    Added methods for result operations in the class

--- data/src/main/java/result/AuthorPaperSubResult.java
+++ data/src/main/java/result/AuthorPaperSubResult.java
@@ -1,12 +1,46 @@
 package result;
 
 import java.util.Collections;
+import java.util.Map;
 import java.util.PriorityQueue;
 
-public class AuthorPaperSubResult {
+public class AuthorPaperSubResult implements Comparable<AuthorPaperSubResult> {
 
-	int authorArticleId;
-	double score;
-	
-	PriorityQueue<KeywordSubResult> keywords = new PriorityQueue<KeywordSubResult>(20, Collections.reverseOrder());
+
+	private int authorArticleId;
+	private double score;
+	PriorityQueue<KeywordSubResult> keywords;
+
+	public AuthorPaperSubResult(int authorArticleId, double authorPaperScore) 
+	{
+		this.authorArticleId = authorArticleId;
+		this.score = authorPaperScore;
+		this.keywords = new PriorityQueue<KeywordSubResult>(20, Collections.reverseOrder());
+	}
+
+	public void addKeywords(Map<Integer, Double> keywordIdToScoreMap)
+	{
+		for(int keywordId : keywordIdToScoreMap.keySet())
+		{
+			double score = keywordIdToScoreMap.get(keywordId);
+			this.keywords.add(new KeywordSubResult(keywordId, score));
+		}
+
+	}
+
+	public int compareTo(AuthorPaperSubResult that) {
+		int result = 0;
+		if(this.score > that.score)
+		{
+			result = 1;
+		}
+		else if(this.score < that.score)
+		{
+			result = -1;
+		}
+
+		return result;
+	}
+
 }
+
