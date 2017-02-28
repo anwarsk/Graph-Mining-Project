c33661d71b0491b55615b987fbe3f5ac699b8cc7
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-02-28 17:56:47 -0500

    Added methods for result operations in the class

--- data/src/main/java/result/ProceeedingPaperSubResult.java
+++ data/src/main/java/result/ProceeedingPaperSubResult.java
@@ -3,10 +3,38 @@ package result;
 import java.util.Collections;
 import java.util.PriorityQueue;
 
-public class ProceeedingPaperSubResult {
-	
-	int proceedingArticleId;
-	double score;
-	
-	PriorityQueue<AuthorPaperSubResult> keywords = new PriorityQueue<AuthorPaperSubResult>(20, Collections.reverseOrder());
+public class ProceeedingPaperSubResult implements Comparable<ProceeedingPaperSubResult> {
+
+	private int proceedingArticleId;
+	private double score;
+
+	private PriorityQueue<AuthorPaperSubResult> authorPaperSubResults;
+
+	public ProceeedingPaperSubResult(int procArticleId) {
+
+		this.proceedingArticleId = procArticleId;
+		authorPaperSubResults = new PriorityQueue<AuthorPaperSubResult>(20, Collections.reverseOrder());
+	}
+
+	public void addAuthorPaperSubResult(AuthorPaperSubResult authorPaperSubResult) {
+
+		this.authorPaperSubResults.add(authorPaperSubResult);
+	}
+
+	public int compareTo(ProceeedingPaperSubResult that) {
+		int result = 0;
+		if(this.score > that.score)
+		{
+			result = 1;
+		}
+		else if(this.score < that.score)
+		{
+			result = -1;
+		}
+		return result;
+	}
+
+	public void setScore(double procPaperScore) {
+		this.score = procPaperScore;
+	}
 }
