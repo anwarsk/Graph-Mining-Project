181005da49bc2e26d8953da8d5e8588832caba72
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-03-01 00:11:58 -0500

    Implemented method to write results to a file

--- analyzer/src/main/java/processor/ResultProcessor.java
+++ analyzer/src/main/java/processor/ResultProcessor.java
@@ -1,11 +1,76 @@
 package processor;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Iterator;
+
+import result.AuthorPaperSubResult;
+import result.KeywordSubResult;
+import result.ProceeedingPaperSubResult;
 import result.Result;
 
 public class ResultProcessor {
 
+	private final String RESULT_FILE_PATH = "D:/Test.csv";
+	
 	public void process(Result result)
 	{
 		assert result !=  null : "Invalid Result";
+		
+		String authorId = result.getAuthorId();
+		int proceedingId = result.getProceedingId();
+		
+		Iterator<ProceeedingPaperSubResult> proceedingPaperIterator = result.getProceedingPaperSubResultIterator();
+		while(proceedingPaperIterator.hasNext())
+		{
+			String resultString = "";
+			resultString += authorId + "," + proceedingId;
+			
+			ProceeedingPaperSubResult procPaperSubResult = proceedingPaperIterator.next();
+			int procArticleId = procPaperSubResult.getProceedingArticleId();
+			double procArticleScore = procPaperSubResult.getScore();
+			resultString += "," + procArticleId + "," + procArticleScore;
+			
+			AuthorPaperSubResult relatedAuthorPaperSubResult = procPaperSubResult.getTopRelatedAuthorPaperSubResult();
+			if(relatedAuthorPaperSubResult == null){continue;}
+			
+			int authorArticleId = relatedAuthorPaperSubResult.getAuthorArticleId();
+			resultString += "," + authorArticleId;
+			
+			String keywordString = "{";
+			Iterator<KeywordSubResult> keywordIterator = relatedAuthorPaperSubResult.getKeywordSubResultIterator();
+			while(keywordIterator.hasNext())
+			{
+				KeywordSubResult keywordSubResult = keywordIterator.next();
+				int keywordId = keywordSubResult.getKeywordId();
+				double keywordScore = keywordSubResult.getScore();
+				
+				keywordString += keywordId + ":" + keywordScore + ";";
+			}
+			
+			keywordString += "}";
+			
+			resultString += "," + keywordString;
+			this.writeToResultFile(resultString);
+		}
+	}
+	
+	private void writeToResultFile(String resultString)
+	{
+		
+		try 
+		{
+			PrintWriter writer;
+			writer = new PrintWriter(new FileOutputStream(new File(this.RESULT_FILE_PATH), true));
+			writer.println(resultString);
+			writer.flush();
+			writer.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
