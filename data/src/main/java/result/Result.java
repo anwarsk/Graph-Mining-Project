8357e40b7262fbaa9c12edb58213b4a5f4c73aae
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-03-01 00:14:03 -0500

    Implemented Getters/Setters for the data

--- data/src/main/java/result/Result.java
+++ data/src/main/java/result/Result.java
@@ -1,6 +1,7 @@
 package result;
 
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.PriorityQueue;
 
@@ -10,21 +11,32 @@ import com.google.common.collect.MinMaxPriorityQueue;
 
 public class Result {
 	
-	
-
 	private String authorId;
 	private int proceedingId;
 	
-	MinMaxPriorityQueue<ProceeedingPaperSubResult> procPaperSubResutls;
+	MinMaxPriorityQueue<ProceeedingPaperSubResult> procPaperSubResults;
 
 	public Result(String authorId, int proceedingId) {
 		
 		this.authorId = authorId;
 		this.proceedingId = proceedingId;
-		procPaperSubResutls = MinMaxPriorityQueue.orderedBy(Collections.reverseOrder()).maximumSize(20).create();
+		procPaperSubResults = MinMaxPriorityQueue.orderedBy(Collections.reverseOrder()).maximumSize(20).create();
+	}
+	
+	public String getAuthorId() {
+		return authorId;
+	}
+
+	public int getProceedingId() {
+		return proceedingId;
 	}
 
 	public void addProceedingPaperSubResult(ProceeedingPaperSubResult procPaperSubResult) {
-		this.procPaperSubResutls.add(procPaperSubResult);
+		this.procPaperSubResults.add(procPaperSubResult);
+	}
+	
+	public Iterator<ProceeedingPaperSubResult> getProceedingPaperSubResultIterator()
+	{
+		return this.procPaperSubResults.iterator();
 	}
 }
