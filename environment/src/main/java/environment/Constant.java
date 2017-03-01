2a46ecef05ae2721165afff826540e75687cf08d
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-02-28 20:10:59 -0500

    Added environment Module with Constants

---
+++ environment/src/main/java/environment/Constant.java
@@ -0,0 +1,7 @@
+package environment;
+
+public class Constant {
+
+	public static final String NEO_GRAPH_DB_PATH = "./../../../Database/graph.acm2015";
+	public static final int MAX_PATH_DEPTH = 6;
+}
