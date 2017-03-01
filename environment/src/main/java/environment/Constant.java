0d73d538b2cdf5fa0ec853bc5286cc4f68f4e8c7
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-03-01 00:11:31 -0500

    Added Constants in the data

--- environment/src/main/java/environment/Constant.java
+++ environment/src/main/java/environment/Constant.java
@@ -2,6 +2,6 @@ package environment;
 
 public class Constant {
 
-	public static final String NEO_GRAPH_DB_PATH = "./../../../Database/graph.acm2015";
+	public static final String NEO_GRAPH_DB_PATH = "./../../../Database/graph.test.acm2015";
 	public static final int MAX_PATH_DEPTH = 6;
 }
