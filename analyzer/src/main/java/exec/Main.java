ade884e2e4a023d9882d1d586b6fdf84d464abb7
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-03-02 20:08:42 -0500

    [Version 1.0] Graph-Miner Running on Azure-VM

--- analyzer/src/main/java/exec/Main.java
+++ analyzer/src/main/java/exec/Main.java
@@ -8,40 +8,43 @@ import java.util.Map.Entry;
 import analyzer.GraphAnalyzer;
 import data.EvaluationInput;
 import db.csv.CSVAccessLayer;
+import environment.Constant;
 import processor.ResultProcessor;
+import processor.ResultSerializer;
 import result.Result;
 
 public class Main {
 
 	//private static final String INPUT_FILE_PATH = "D:\\Work\\Database\\CSV\\evaluation_aid_proc_id_gt_5.csv";
-	private static final String INPUT_FILE_PATH = "D:\\Work\\Database\\CSV\\evaluation_aid_proc_id_gt_5 _test.csv";
-	
+	private static final String INPUT_FILE_PATH = Constant.EVALUATION_INPUT_FILE;
+
 	public static void main(String[] args) {
-	
+
 		// Read the evaluation file
 		CSVAccessLayer csvAccessLayer = new CSVAccessLayer();
 		EvaluationInput evaluationInput = csvAccessLayer.readEvaluationInput(INPUT_FILE_PATH);
 		System.out.println(evaluationInput.size());
-		
-		
+
+
 		GraphAnalyzer analyzer = new GraphAnalyzer();
 		List<Result> results = new ArrayList<Result>();
-		
+
 		ResultProcessor resultProcessor = new ResultProcessor();
+		ResultSerializer resultSerializer = new ResultSerializer();
 		// Send the input to analyzer for analysis
 		Iterator<Entry<String, Integer>> inputIterator = evaluationInput.getIterator();
 		while(inputIterator.hasNext())
 		{
 			Entry<String, Integer> entry = inputIterator.next();
-			String authorId = "a_14178";//entry.getKey();
-			int proceedingId = 1289612; //entry.getValue();
-			
+			String authorId = entry.getKey(); //"a_37712";
+			int proceedingId = entry.getValue(); //1148170;
+
+			System.out.println(String.format("Proceessing for author: %s | Conference: %d", authorId, proceedingId ));
 			// find nodes between these ids
 			Result result = analyzer.generateResults(authorId, proceedingId);
-			
+
 			resultProcessor.process(result);
-			results.add(result);
-			return;
+			resultSerializer.writeResultToFile(result);
 		}
 	}
 
