1c3d9c392514b52a6d2f16e2fe7627254388b307
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-03-02 20:06:18 -0500

    Implemented serializable interface

--- data/src/main/java/result/AuthorPaperSubResult.java
+++ data/src/main/java/result/AuthorPaperSubResult.java
@@ -1,11 +1,12 @@
 package result;
 
+import java.io.Serializable;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.Map;
 import com.google.common.collect.MinMaxPriorityQueue;
 
-public class AuthorPaperSubResult implements Comparable<AuthorPaperSubResult> {
+public class AuthorPaperSubResult implements Serializable, Comparable<AuthorPaperSubResult> {
 
 
 	private int authorArticleId;
