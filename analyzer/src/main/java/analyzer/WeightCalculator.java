b35f659c363c535d51fa9f4598d0cbae144f1aad
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-03-05 18:19:43 -0500

    Updated relationship weights for b_weight instead of f_weight

--- analyzer/src/main/java/analyzer/WeightCalculator.java
+++ analyzer/src/main/java/analyzer/WeightCalculator.java
@@ -1,13 +1,34 @@
 package analyzer;
 
+import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.RelationshipType;
 
 public class WeightCalculator {
 
 	public double getWeightForRelation(Relationship relation)
 	{
 		assert relation != null : "Null Relation";
-		double weight = (Double) relation.getProperty("f_weight");
+		
+		double weight = 1;
+		
+		if(relation.getType().name().compareToIgnoreCase("cite") == 0)
+		{
+				weight = (Double) relation.getProperty("b_weight");
+		}
+		else if(relation.getType().name().compareToIgnoreCase("contains") == 0)
+		{
+				weight = (Double) relation.getProperty("b_weight");
+		}
+		else if(relation.getType().name().compareToIgnoreCase("written") == 0)
+		{
+				weight = (Double) relation.getProperty("b_weight");
+		}
+		else
+		{
+			throw new IllegalStateException();
+		}
+			
 		return weight;
 	}
 }
