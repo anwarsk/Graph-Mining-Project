6e1a039b8456f41122baf16dcf8e8106f9293b6c
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-03-05 18:20:18 -0500

    Updated node filter to filter out papers published prior to conference

--- analyzer/src/main/java/analyzer/NodeFilter.java
+++ analyzer/src/main/java/analyzer/NodeFilter.java
@@ -8,12 +8,34 @@ import org.neo4j.graphdb.Node;
 // Node Filter just traversing paper and keyword
 public class NodeFilter implements Predicate<Node>{
 
+	private long cutOffDate;
+	private long startNodeId;
+	private long endNodeId;
+	
+	public NodeFilter(long cutOffDate, long startNodeId, long endNodeId)
+	{
+		this.cutOffDate = cutOffDate;
+		this.startNodeId = startNodeId;
+		this.endNodeId = endNodeId;
+	}
+
 	@Override
 	public boolean test(Node testNode) {
 		// TODO Auto-generated method stub
 		boolean isNodeMatch = false;
 
-		if(testNode.hasLabel(Label.label("paper")) | testNode.hasLabel(Label.label("keyword")))
+		if(testNode.hasLabel(Label.label("paper"))) 
+		{
+			if(/*testNode.getId() == startNodeId |*/ testNode.getId()== endNodeId)
+			{
+				isNodeMatch = true;
+			}
+			else if(((long)testNode.getProperty("publication_date")) < cutOffDate)
+			{
+				isNodeMatch = true;
+			}
+		}
+		else if(testNode.hasLabel(Label.label("author")) | testNode.hasLabel(Label.label("keyword")))
 		{
 			isNodeMatch = true;
 		}
