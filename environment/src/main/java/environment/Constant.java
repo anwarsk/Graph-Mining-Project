f609918d3ff9c2e2c0046089eafda69d123c8dd8
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-03-05 00:31:08 -0500

    Added new constants
    - To hold starting path length
    - To hold max path length
    - To hold max number of paths to traverse

--- environment/src/main/java/environment/Constant.java
+++ environment/src/main/java/environment/Constant.java
@@ -2,18 +2,23 @@ package environment;
 
 public class Constant {
 
-	public static final String NEO_GRAPH_DB_PATH = "./../../../Database/graph.test.acm2015";
-	//public static final String NEO_GRAPH_DB_PATH = "/home/anwar/project/data/graph.acm2015";
-	public static final int MAX_PATH_DEPTH = 6;
+	//public static final String NEO_GRAPH_DB_PATH = "./../../../Database/graph.test.acm2015";
+	public static final String NEO_GRAPH_DB_PATH = "/home/anwar/project/data/graph.acm2015";
 	
 	
-	public static final String JSON_OUTPUT_DIRECTORY = "D:/output/json";
-	//public static final String JSON_OUTPUT_DIRECTORY = "/home/anwar/project/output/json";
+	
+	//public static final String JSON_OUTPUT_DIRECTORY = "D:/output/json";
+	public static final String JSON_OUTPUT_DIRECTORY = "/home/anwar/project/output/json";
+	
 	public static final String JSON_FILE_NAME_FORMAT = "%s_%d.json"; 
 	
-	public static final String EVALUATION_INPUT_FILE = "C:\\Users\\send2\\Desktop\\evaluation_data_gt_4_id.csv";
-	//public static final String EVALUATION_INPUT_FILE = "/home/anwar/project/input/evaluation_data_gt_4_id.csv";
+	//public static final String EVALUATION_INPUT_FILE = "C:\\Users\\send2\\Desktop\\evaluation_data_gt_4_id.csv";
+	public static final String EVALUATION_INPUT_FILE = "/home/anwar/project/input/evaluation_data_gt_4_id.csv";
 	
-	public static final String EVALUATION_OUTPUT_FILE = "D:/results.csv";
-	//public static final String EVALUATION_OUTPUT_FILE = "/home/anwar/project/output/results.csv";
+	//public static final String EVALUATION_OUTPUT_FILE = "D:/results.csv";
+	public static final String EVALUATION_OUTPUT_FILE = "/home/anwar/project/output/results.csv";
+	
+	public static final int START_PATH_DEPTH = 2;
+	public static final long MAX_PATHS = 10000;
+	public static final int MAX_PATH_DEPTH = 6;
 }
