818bcf1d51a7ef9f0e206e988b35e36fc51234e7
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-03-14 00:05:46 -0400

    Added class to read result jason file

---
+++ dbmanager/src/main/java/db/json/JSONAccessLayer.java
@@ -0,0 +1,17 @@
+package db.json;
+
+import result.Result;
+
+public class JSONAccessLayer {
+
+	Result readResultFromJSONFile(String jsonFile)
+	{
+		assert jsonFile.isEmpty() == false : "Empty file path";
+		
+		Result result = null;
+		
+		
+		
+		return result;
+	}
+}
