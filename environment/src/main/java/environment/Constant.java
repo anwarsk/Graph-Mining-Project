64dc5d5a70fa9db5ada8755382579397a82b5d54
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-03-14 02:13:30 -0400

    Added new constants for values

--- environment/src/main/java/environment/Constant.java
+++ environment/src/main/java/environment/Constant.java
@@ -3,17 +3,17 @@ package environment;
 public class Constant {
 
 	
-	/**    AZURE */
+	/**    AZURE 
 	public static final String NEO_GRAPH_DB_PATH = "/home/anwar/project/database/graph.acm2015";
 	public static final String JSON_OUTPUT_DIRECTORY = "/home/anwar/project/output/output_v6.0_gt_3/json";
 	public static final String EVALUATION_INPUT_FILE = "/home/anwar/project/input/evaluation_data_gt_3_id.csv";
 	public static final String EVALUATION_OUTPUT_FILE = "/home/anwar/project/output/output_v6.0_gt_3/results.csv";
-	//*/
+	// */
 	
-	 /*
+	 /// *
 	public static final String NEO_GRAPH_DB_PATH = "./../../../Database/graph.acm2015";
-	public static final String JSON_OUTPUT_DIRECTORY = "D:/output/json";	
-	public static final String EVALUATION_INPUT_FILE = "C:\\Users\\send2\\Desktop\\evaluation_data_gt_4_id.csv";
+	public static final String JSON_OUTPUT_DIRECTORY = "D:\\Work\\Results\\GC-Results\\output_v5.4_gt_3\\json";	
+	public static final String EVALUATION_INPUT_FILE = "D:\\Work\\Database\\CSV\\evaluationData\\evaluation_data_gt_3_id.csv";
 	public static final String EVALUATION_OUTPUT_FILE = "D:/results.csv";
 	//*/
 	
@@ -22,4 +22,5 @@ public class Constant {
 	public static final long MAX_PATHS = 1000000;
 	public static final int MAX_PATH_DEPTH = 8;
 	public static final double RANDON_WALK_PROB_CUTOFF = 1e-80;
+	public static final String EVALUATION_RESULT_FILE = "D:\\Work\\Results\\GC-Results\\output_v5.4_gt_3\\FinalResult.csv";
 }
