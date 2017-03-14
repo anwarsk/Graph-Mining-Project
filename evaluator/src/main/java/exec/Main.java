21a61cc8eb51e4f216341f5e1f37d1dbf9d66467
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-03-14 02:15:59 -0400

    [Completed] code for evaluation of Papers

--- evaluator/src/main/java/exec/Main.java
+++ evaluator/src/main/java/exec/Main.java
@@ -1,89 +1,14 @@
 package exec;
 
-import java.io.File;
-import java.io.FileReader;
-import java.lang.reflect.Type;
-import java.nio.CharBuffer;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map.Entry;
 
-import com.google.common.collect.MinMaxPriorityQueue;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
-
-import result.AuthorPaperSubResult;
-import result.KeywordSubResult;
-import result.ProceeedingPaperSubResult;
+import data.EvaluationInput;
+import db.csv.CSVAccessLayer;
+import db.json.JSONAccessLayer;
+import environment.Constant;
+import evaluator.Evaluator;
 import result.Result;
-
-
-class PQDeserializer<E> implements JsonDeserializer<MinMaxPriorityQueue> {
-	  public MinMaxPriorityQueue<ProceeedingPaperSubResult> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
-	      throws JsonParseException {
-		  MinMaxPriorityQueue<ProceeedingPaperSubResult> priorityQueue = MinMaxPriorityQueue.orderedBy(Collections.reverseOrder()).maximumSize(20).create();
-		  Iterator<JsonElement> j = json.getAsJsonArray().iterator();
-		  while (j.hasNext())
-		  {
-			  JsonElement jsonElement = j.next();
-			  ProceeedingPaperSubResult proceedingPaperSubResult = this.getProceedingPaperSubResult(jsonElement);
-			  priorityQueue.add(proceedingPaperSubResult);
-		  }
-//		  System.out.println(j.get(0).getAsJsonObject().get("proceedingArticleId"));
-//		  System.out.println(j.get(0).toString() + "\n");
-//		  System.out.println(json.toString() + "\n");
-	    return priorityQueue;  
-	  }
-	  
-	  private ProceeedingPaperSubResult getProceedingPaperSubResult(JsonElement jsonElement)
-	  {
-		  JsonObject object = jsonElement.getAsJsonObject();
-		  int proceedingArticleId = Integer.parseInt(object.get("proceedingArticleId").toString());
-		  double score =  Double.parseDouble(object.get("score").toString());
-		  ProceeedingPaperSubResult proceedingPaperSubResult = new ProceeedingPaperSubResult(proceedingArticleId);
-		  proceedingPaperSubResult.setScore(score);
-		  Iterator<JsonElement> element = object.get("authorPaperSubResults").getAsJsonArray().iterator();
-		  while(element.hasNext())
-		  {
-			  JsonElement authorPaperSubResultElement = element.next();
-			  AuthorPaperSubResult authorPaperSubResult = this.getAuthorPaperSubResult(authorPaperSubResultElement);
-			  proceedingPaperSubResult.addAuthorPaperSubResult(authorPaperSubResult);
-		  }
-		  
-		  return proceedingPaperSubResult;
-		  
-	  }
-	  
-	  private AuthorPaperSubResult getAuthorPaperSubResult(JsonElement jsonElement)
-	  {
-		  JsonObject object = jsonElement.getAsJsonObject();
-		  int authorArticleId = Integer.parseInt(object.get("authorArticleId").toString());
-		  double score =  Double.parseDouble(object.get("score").toString());
-		  AuthorPaperSubResult authorPaperSubResult = new AuthorPaperSubResult(authorArticleId, score);
-		  Iterator<JsonElement> element = object.get("keywords").getAsJsonArray().iterator();
-		  Gson gson = new Gson();
-
-		  while(element.hasNext())
-		  {
-			  JsonElement j = element.next();
-			  int keywordId = j.getAsJsonObject().get("keywordId").getAsInt();
-			  double score1 =  j.getAsJsonObject().get("score").getAsDouble();
-			  KeywordSubResult ksbrs = new KeywordSubResult(keywordId, score1);
-			  authorPaperSubResult.addKeyword(ksbrs);
-		  }
-		  
-		  return authorPaperSubResult;
-	  }
-	}
-
 
 public class Main {
 
@@ -91,15 +16,28 @@ public class Main {
 		// TODO Auto-generated method stub
 		try
 		{
-			String jsonFilePath = "D:\\Work\\Code\\json_result\\a_320547_1736020.json";
-			String jsonContent = new String(Files.readAllBytes(Paths.get(jsonFilePath)));
-			
-			System.out.println(jsonContent.toString());
+			CSVAccessLayer csvAccessLayer = new CSVAccessLayer();
+			EvaluationInput evaluationInput = csvAccessLayer.readEvaluationInput(Constant.EVALUATION_INPUT_FILE);
+			System.out.println("INPUT SIZE: " + evaluationInput.size());
 
-			GsonBuilder gsonBuilder = new GsonBuilder();
-			gsonBuilder.registerTypeAdapter(MinMaxPriorityQueue.class, new PQDeserializer());
-			Gson gson = gsonBuilder.create();
-			Result r = gson.fromJson(jsonContent.toString(), Result.class);
+			JSONAccessLayer jsonAccessLayer = new JSONAccessLayer();
+			
+			Iterator<Entry<String, Integer>> inputIterator = evaluationInput.getIterator();
+			String jsonFileDirectory = Constant.JSON_OUTPUT_DIRECTORY;
+			Evaluator.initialize(Constant.EVALUATION_RESULT_FILE);
+			while(inputIterator.hasNext())
+			{
+				Entry<String, Integer> input = inputIterator.next();
+				String jsonFileName = String.format(Constant.JSON_FILE_NAME_FORMAT, input.getKey(), input.getValue());
+				String jsonFilePath = jsonFileDirectory + "/" + jsonFileName;
+				Result result = jsonAccessLayer.readResultFromJSONFile(jsonFilePath);
+				Evaluator evaluator = new Evaluator();
+				evaluator.evaluate(result);
+				evaluator.writeLocalResults();
+			}
+			
+			Evaluator.writeGlobalResults();
+			 
 			System.out.println("Completed.");
 			
 			
