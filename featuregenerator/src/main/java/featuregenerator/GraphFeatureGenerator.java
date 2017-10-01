cb74edd3036e7ccf81f2cf5b2aa7b3938b46b4e7
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-09-30 21:31:32 -0400

    Added FeatureGenerator module to the project
    -It will be used to generate all the features required for PU-Learning
    -Added class GraphFeatureGenerator

---
+++ featuregenerator/src/main/java/featuregenerator/GraphFeatureGenerator.java
@@ -0,0 +1,11 @@
+package featuregenerator;
+
+/**
+ * This class is responsible for generating all the graph related features from the
+ * Neo4j graph.
+ * @author anwar
+ *
+ */
+public class GraphFeatureGenerator {
+
+}
