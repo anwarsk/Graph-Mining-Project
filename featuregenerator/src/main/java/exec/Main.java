1511cb9b1ebbb0c4cecd2f5d83f8b434841edd53
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-10-05 18:14:16 -0400

    Updated the main method to write result

--- featuregenerator/src/main/java/exec/Main.java
+++ featuregenerator/src/main/java/exec/Main.java
@@ -2,6 +2,7 @@ package exec;
 
 import data.EvaluationInput;
 import data.FeatureGeneratorInput;
+import data.FeatureGeneratorOutput;
 import db.csv.CSVAccessLayer;
 import environment.Constant;
 import featuregenerator.GraphFeatureGenerator;
@@ -12,11 +13,13 @@ public class Main {
 		
 		// (1) - Read the feature generator input
 		CSVAccessLayer csvAccessLayer = new CSVAccessLayer();
-		FeatureGeneratorInput featureGeneratorInput = csvAccessLayer.readFeatureGenerationInput(Constant.FEATURE_GENERATOR_INPUT_FILE);
+		FeatureGeneratorInput featureGeneratorInput = csvAccessLayer.readFeatureGeneratorInput(Constant.FEATURE_GENERATOR_INPUT_FILE);
 		
 		// (2) - Generate distance features 
 		GraphFeatureGenerator graphFeatureGenerator = new GraphFeatureGenerator();
-		graphFeatureGenerator.generateDistanceFeature(featureGeneratorInput);
+		FeatureGeneratorOutput featureGeneratorOutput = graphFeatureGenerator.generateDistanceFeature(featureGeneratorInput);
+		
+		csvAccessLayer.writeFeatureGeneratorOutput(Constant.FEATURE_GENERATOR_OUTPUT_FILE, featureGeneratorOutput);
 	}
 
 }
