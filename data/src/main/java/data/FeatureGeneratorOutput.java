ab284deba7b94701b32bbaecce50d8c5548a91f7
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-10-05 17:26:35 -0400

    Added a FeatureGeneratorOutput singleton Class to hold results

---
+++ data/src/main/java/data/FeatureGeneratorOutput.java
@@ -0,0 +1,64 @@
+package data;
+
+import java.util.HashMap;
+
+public class FeatureGeneratorOutput {
+	
+	private HashMap<String, FeatureEntry> featureOutput;
+	private static FeatureGeneratorOutput instance  = null;
+	
+	private FeatureGeneratorOutput()
+	{
+		
+	}
+	
+	public static FeatureGeneratorOutput getInstance()
+	{
+		if (instance == null)
+		{
+			instance = new FeatureGeneratorOutput();
+		}
+		
+		return instance;
+	}
+	
+	FeatureEntry getFeatureEntry(String authorId, int articleId)
+	{
+		assert authorId != null && authorId.isEmpty() == false : "Invalid author Id";
+		assert articleId > 0 : "Invalid Article Id";
+		
+		FeatureEntry featureEntry = null;
+		
+		String hash = this.getHash(authorId, articleId);
+		featureEntry = this.featureOutput.get(hash);
+		
+		if(featureEntry == null)
+		{
+			featureEntry = new FeatureEntry();
+			featureEntry.articleId = articleId;
+			featureEntry.authorId = authorId;
+			featureOutput.put(hash, featureEntry);
+		}
+		
+		return featureEntry;
+	}
+	
+	private String getHash(String authorId, int articleId)
+	{
+		return authorId + articleId;
+	}
+	
+	public void addDistance(String authorId, int articleId, int distance)
+	{
+		FeatureEntry featureEntry = this.getFeatureEntry(authorId, articleId);
+		featureEntry.distance = distance;
+	}
+
+}
+
+class FeatureEntry
+{
+	String authorId;
+	int articleId;
+	int distance;
+}
