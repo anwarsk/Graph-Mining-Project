2976e0ad3714d24101d04e0ee41ff249b2355752
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-10-05 23:53:21 -0400

    Added method getRandomWalkProbability(..)

--- featuregenerator/src/main/java/featuregenerator/GraphFeatureGenerator.java
+++ featuregenerator/src/main/java/featuregenerator/GraphFeatureGenerator.java
@@ -2,11 +2,13 @@ package featuregenerator;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.neo4j.cypher.internal.frontend.v2_3.perty.print.condense;
 import org.neo4j.graphalgo.GraphAlgoFactory;
 import org.neo4j.graphalgo.PathFinder;
 import org.neo4j.graphdb.GraphDatabaseService;
@@ -20,6 +22,7 @@ import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
+import analyzer.WeightCalculator;
 import data.FeatureGeneratorInput;
 import data.FeatureGeneratorOutput;
 import db.csv.CSVAccessLayer;
@@ -71,8 +74,8 @@ public class GraphFeatureGenerator {
 				int proceedingId = input.getValue();
 
 				// (1) - Find the node in the graph for the author 
-				Node author = dbService.findNode(Label.label("author"), "author_id", authorId);
-				if(author == null){ System.out.println("Can not find author with id-" + authorId); continue;}
+				Node authorNode = dbService.findNode(Label.label("author"), "author_id", authorId);
+				if(authorNode == null){ System.out.println("Can not find author with id-" + authorId); continue;}
 
 				// (2) -  
 				Node proceeding = dbService.findNode(Label.label("proceeding"), "proc_id", proceedingId);
@@ -90,9 +93,14 @@ public class GraphFeatureGenerator {
 					if(paperNode == null){System.out.println("Null paper Node from the relationship"); continue;}
 
 					int articleId = (int) (long) paperNode.getProperty("article_id");
-					int shortestDistance = this.getShortestDistanceBetweenAuthorAndPaper(author, paperNode);
+					
+					int shortestDistance = 0; //this.getShortestDistanceBetweenAuthorAndPaper(authorNode, paperNode);
+					double randomWalkProbability = this.getRandomWalkProbability(authorNode, paperNode);
 
-					featureGeneratorOutput.addDistance(authorId, articleId, shortestDistance);
+					//featureGeneratorOutput.addDistance(authorId, articleId, shortestDistance);
+					featureGeneratorOutput.addFeatures(authorId, articleId, 
+													   shortestDistance, randomWalkProbability);
+					
 				}
 			}
 
@@ -124,13 +132,53 @@ public class GraphFeatureGenerator {
 
 		// 
 		PathFinder<Path> pathFinder = GraphAlgoFactory.shortestPath(pathExpander, 
-																	Constant.MAX_PATH_DEPTH, 
-																	Constant.SHORTEST_PATH_COUNT);
+				Constant.MAX_PATH_DEPTH, 
+				Constant.SHORTEST_PATH_COUNT);
 
 		Path path = pathFinder.findSinglePath(author, paper);
 		if( path == null) { System.out.println("Can not find path between a-p"); return -1;}
 		distance = path.length();
 		return distance;
+	}
+
+	double getRandomWalkProbability(Node author, Node paper)
+	{
+		assert author != null : "Null Author node";
+		assert paper != null : "Null Paper node";
+
+		if(!isInitialized){intialize();}
+
+		double randomWalkProbability = 0;
+		double currentScoringMethod = 0;
+		Map<Integer, Integer> pathLengthToCountMap = new HashMap<Integer, Integer>();
+		
+		WeightCalculator weightCalculator = new WeightCalculator();
+
+		// (3) - Get the path finder 
+		PathExpander<Object> pathExpander = this.createPathExpanderForPaperAndKeyword();
+
+		// 
+		PathFinder<Path> pathFinder = GraphAlgoFactory.allPaths(pathExpander, Constant.MAX_PATH_DEPTH);
+
+		Iterable<Path> allPaths = pathFinder.findAllPaths(author, paper);
+		
+		if(allPaths == null || allPaths.iterator().hasNext() == false) {System.out.println("No Path between Author and Paper");}
+		
+		for(Path path : allPaths)
+		{
+			double pathRandomWalkProbability = 1;
+			if(path == null) { System.out.println("Null Path");continue;}
+			
+			for(Relationship realtionship : path.relationships())
+			{
+				if(realtionship == null) {continue;}
+				pathRandomWalkProbability *= weightCalculator.getWeightForRelation(realtionship);
+			}
+			
+			randomWalkProbability += pathRandomWalkProbability;
+		}
+		
+		return randomWalkProbability;
 	}
 
 	private PathExpander<Object> createPathExpanderForPaperAndKeyword()
