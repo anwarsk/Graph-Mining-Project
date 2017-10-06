7769f6be10382195dd5c2d38404fda864f710752
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-10-06 02:00:28 -0400

    Added CurrentScoringMethod and PathLengthToCount map

--- data/src/main/java/data/FeatureEntry.java
+++ data/src/main/java/data/FeatureEntry.java
@@ -1,8 +1,12 @@
 package data;
 
+import java.util.Map;
+
 public class FeatureEntry {
 	public String authorId;
 	public int articleId;
 	public int distance;
 	public double randomWalkProbability;
+	public double currentScoringMethod = 0;
+	public Map<Integer, Integer> pathLengthToCountMap = null;
 }
