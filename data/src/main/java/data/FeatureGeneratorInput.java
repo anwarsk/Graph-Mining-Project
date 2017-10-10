2f217ffef1d0933cbbbb6140276da960684c9e28
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-10-10 12:55:10 -0400

    Added structure to hold the already processed pairs of author-proceeding

--- data/src/main/java/data/FeatureGeneratorInput.java
+++ data/src/main/java/data/FeatureGeneratorInput.java
@@ -1,5 +1,40 @@
 package data;
 
+import java.util.HashSet;
+
 public class FeatureGeneratorInput extends EvaluationInput {
 
+	HashSet<String> processedInput;
+
+	public FeatureGeneratorInput() {
+		this.processedInput = new HashSet<String>();
+	}
+
+	private String getHash(String authorId, int proceedingId)
+	{
+		return authorId + proceedingId;
+	}
+
+	public void addProcessedInput(String authorId, int proceedingId)
+	{
+		processedInput.add(this.getHash(authorId, proceedingId));
+	}
+
+	public boolean isProcessed(String authorId, int proceedingId)
+	{
+		return processedInput.contains(this.getHash(authorId, proceedingId));
+	}
+
+	@Override
+	public void addEntry(String authorId, Integer proceedingId) {
+		// TODO Auto-generated method stub
+		if(!this.isProcessed(authorId, proceedingId))
+		{
+			super.addEntry(authorId, proceedingId);
+		}
+		else
+		{
+			System.out.println("AuthorId, ProceedingId alreadyProcessed-" + authorId + ", " + proceedingId);
+		}
+	}
 }
