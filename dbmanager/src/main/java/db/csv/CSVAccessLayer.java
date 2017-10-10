7f0e097b0b248209b5b27c13048578a163288195
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2017-10-10 12:57:22 -0400

    Updated to read/write intermediate results from feature Generator

--- dbmanager/src/main/java/db/csv/CSVAccessLayer.java
+++ dbmanager/src/main/java/db/csv/CSVAccessLayer.java
@@ -1,9 +1,11 @@
 package db.csv;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
@@ -103,21 +105,21 @@ public class CSVAccessLayer {
 		EvaluationInput evaluationInput = new EvaluationInput();
 
 		String line = "";
-		
+
 		try (BufferedReader buffReader = new BufferedReader(new FileReader(inputFilePath))) 
 		{
 			buffReader.readLine();
-			
+
 			while ((line = buffReader.readLine()) != null) {
 
 				// use comma as separator
 				String[] data = line.split(",");
-				
+
 				String authorId = data[Constant.GROUND_TRUTH_AUTHOR_ID_COLUMN_INDEX];
 				int proceedingId = Integer.parseInt(data[Constant.GROUND_TRUTH_PROCEEDING_ID_COLUMN_INDEX]);
-				
+
 				evaluationInput.addEntry(authorId, proceedingId);
-				
+
 			}
 
 		} catch (FileNotFoundException e) {
@@ -131,30 +133,28 @@ public class CSVAccessLayer {
 
 		return evaluationInput;
 	}
-	
-	public FeatureGeneratorInput readFeatureGeneratorInput(String inputFilePath)
+
+	public void readFeatureGeneratorProcessedLog(String processedLogFilePath, FeatureGeneratorInput featureGeneratorInput)
 	{
-		assert inputFilePath != null : "Null input file path";
-		assert inputFilePath != "" : "Empty input file path";
-
-		FeatureGeneratorInput featureGeneratorInput = new FeatureGeneratorInput();
-
-		String line = "";
+		assert processedLogFilePath != null : "Null input file path";
+		assert processedLogFilePath != "" : "Empty input file path";
 		
-		try (BufferedReader buffReader = new BufferedReader(new FileReader(inputFilePath))) 
+		String line = "";
+
+		try (BufferedReader buffReader = new BufferedReader(new FileReader(processedLogFilePath))) 
 		{
 			buffReader.readLine();
-			
+
 			while ((line = buffReader.readLine()) != null) {
 
 				// use comma as separator
 				String[] data = line.split(",");
-				
+
 				String authorId = data[Constant.FEATURE_GENERATOR_AUTHOR_ID_COLUMN_INDEX];
 				int proceedingId = Integer.parseInt(data[Constant.FEATURE_GENERATOR_PROCEEDING_ID_COLUMN_INDEX]);
-				
-				featureGeneratorInput.addEntry(authorId, proceedingId);
-				
+
+				featureGeneratorInput.addProcessedInput(authorId, proceedingId);
+
 			}
 
 		} catch (FileNotFoundException e) {
@@ -164,32 +164,67 @@ public class CSVAccessLayer {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-
-
-		return featureGeneratorInput;
 	}
 	
-	public void writeFeatureGeneratorOutput(String outputFilePath, FeatureGeneratorOutput featureGeneratorOutput)
+	public void readFeatureGeneratorInput(String inputFilePath, FeatureGeneratorInput featureGeneratorInput)
+	{
+		assert inputFilePath != null : "Null input file path";
+		assert inputFilePath != "" : "Empty input file path";
+
+		String line = "";
+
+		try (BufferedReader buffReader = new BufferedReader(new FileReader(inputFilePath))) 
+		{
+			buffReader.readLine();
+
+			while ((line = buffReader.readLine()) != null) {
+
+				// use comma as separator
+				String[] data = line.split(",");
+
+				String authorId = data[Constant.FEATURE_GENERATOR_AUTHOR_ID_COLUMN_INDEX];
+				int proceedingId = Integer.parseInt(data[Constant.FEATURE_GENERATOR_PROCEEDING_ID_COLUMN_INDEX]);
+
+				featureGeneratorInput.addEntry(authorId, proceedingId);
+
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+
+	public synchronized void writeFeatureGeneratorOutput(String outputFilePath, FeatureGeneratorOutput featureGeneratorOutput)
 	{
 		assert outputFilePath != null : "Null output file path";
 		assert outputFilePath != "" : "Empty output file path";
 		assert featureGeneratorOutput != null : "Null featureGeneratorOutput object";
-		
+
 		PrintWriter fileWriter;
 		try {
-			fileWriter = new PrintWriter(new File(outputFilePath));
-			String pathLengthCountHeader = "";
-			for(int pathLength=1; pathLength <= Constant.MAX_PATH_DEPTH; pathLength++)
+			File outputFile = new File(outputFilePath);
+			boolean isOutputFileExists = outputFile.exists();
+			fileWriter = new PrintWriter(new BufferedWriter(new FileWriter(outputFile, true)));
+			if(!isOutputFileExists)
 			{
-				pathLengthCountHeader += ",PathLength=" + pathLength;  
+				outputFile.createNewFile();
+				String pathLengthCountHeader = "";
+				for(int pathLength=1; pathLength <= Constant.MAX_PATH_DEPTH; pathLength++)
+				{
+					pathLengthCountHeader += ",PathLength=" + pathLength;  
+				}
+
+				fileWriter.write("AuthorId,ArticleId,"
+						+ "ShortestDistance,"
+						+ "RandomWalkProbability,"
+						+ "CurrentScoringMethod"
+						+ pathLengthCountHeader 
+						+ "\n");
 			}
-			
-			fileWriter.write("AuthorId,ArticleId,"
-					+ "ShortestDistance,"
-					+ "RandomWalkProbability,"
-					+ "CurrentScoringMethod"
-					+ pathLengthCountHeader 
-					+ "\n");
 			for (FeatureEntry featureEntry : featureGeneratorOutput.getListOfFeatureEntry())
 			{
 				String pathLengthCountString = "";
@@ -198,13 +233,13 @@ public class CSVAccessLayer {
 				{
 					pathLengthCountString += "," + pathLengthToCountMap.getOrDefault(pathLength, 0); 
 				}
-				
+
 				String line = featureEntry.authorId + "," + 
-							  featureEntry.articleId + "," + 
-							  featureEntry.distance + "," +
-							  featureEntry.randomWalkProbability + "," +
-							  featureEntry.currentScoringMethod +
-							  pathLengthCountString;
+						featureEntry.articleId + "," + 
+						featureEntry.distance + "," +
+						featureEntry.randomWalkProbability + "," +
+						featureEntry.currentScoringMethod +
+						pathLengthCountString;
 				fileWriter.println(line);
 			}
 			fileWriter.flush();
@@ -212,9 +247,38 @@ public class CSVAccessLayer {
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
+
 	}
 
+	public synchronized void writeFeatureGeneratorProcessLog(String featureGeneratorProcessLogFilePath, String authorId2,
+															int proceedingId2) {
+		
+		assert featureGeneratorProcessLogFilePath != null : "Null output file path";
+		assert featureGeneratorProcessLogFilePath != "" : "Empty output file path";
 
+		PrintWriter fileWriter;
+		try {
+			File outputFile = new File(featureGeneratorProcessLogFilePath);
+			boolean isOutputFileExists = outputFile.exists();
+			fileWriter = new PrintWriter(new BufferedWriter(new FileWriter(outputFile, true)));
+			if(!isOutputFileExists)
+			{
+				outputFile.createNewFile();
+				fileWriter.write("AuthorId,ProceedingId\n");
+			}
+			fileWriter.println(authorId2 + "," + proceedingId2);
+			fileWriter.flush();
+			fileWriter.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
